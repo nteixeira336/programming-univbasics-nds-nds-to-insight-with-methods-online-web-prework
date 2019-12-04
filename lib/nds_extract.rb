01a215c15b63b99941ecb627b6851e59c7fa7f61
Ninibeth Teixeira <ninibethreina@gmail.com>
2019-12-04 15:30:16 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -7,7 +7,7 @@ require 'directors_database'
 # { directorOne => allTheMoneyTheyMade, ... }
 
 def directors_totals(nds)
-  binding.pry 
+  #binding.pry 
   directors_totals = { }
   directors_index=0 
   while directors_index < nds.length do 
