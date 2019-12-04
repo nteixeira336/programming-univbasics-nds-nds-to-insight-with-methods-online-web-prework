2e0b2746f53c6eb4a725016b693b485dd32f0417
Ninibeth Teixeira <ninibethreina@gmail.com>
2019-12-04 15:52:54 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -27,4 +27,5 @@ def directors_totals(nds)
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
+  binding.pry 
 end
