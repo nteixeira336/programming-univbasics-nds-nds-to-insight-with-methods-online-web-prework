92fcc5435f014b3f68ef882e44a5e5c1b70a684a
Ninibeth Teixeira <ninibethreina@gmail.com>
2019-12-04 15:16:51 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -7,11 +7,22 @@ require 'directors_database'
 # { directorOne => allTheMoneyTheyMade, ... }
 
 def directors_totals(nds)
+  directors_totals = { }
+  directors_index=0 
+  while directors_index < nds.length do 
+  
+  director= nds[directors_index][:name]
+  directors_totals[director]=0 
+  worldwide_gross_index=0 
+    while worldwide_gross_index < nds[directors_index][:movies].length do 
+      
+    directors_totals+=nds[directors_index][:movies][worldwide_gross_index][:worldwide_gross]
+    worldwide_gross_index+=1 
+    end 
+  directors_index+=1 
+  end 
   binding.pry 
-  directors_totals = {
-    
-  }
-  directors_database
+  directors_totals 
 end
 
 # Find a way to accumulate the :worldwide_grosses and return that Integer
